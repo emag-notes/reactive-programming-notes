d9e25da3ef0f0dfa603cd7ed7864dd5f8d5f5509
emag <lanabe.lanabe@gmail.com>
2016-04-27 23:09:16 +0900

    New requests from refresh clicks

---
+++ 06-new-requests-from-refresh-clicks/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var refreshButton = document.querySelector('.refresh');
+
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+var startupRequestStream = Rx.Observable.just('https://api.github.com/users');
+
+var requestOnRefreshStream = refreshClickStream
+  .map(event => {
+    var randomOffset = Math.floor(Math.random() * 500);
+    return 'https://api.github.com/users?since=' + randomOffset;
+  });
+
+var responseStream = startupRequestStream.merge(requestOnRefreshStream)
+  .flatMap(requestUrl =>
+    Rx.Observable.fromPromise($.getJSON(requestUrl))
+  );
+
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser =>
+    listUser[Math.floor(Math.random() * listUser.length)]
+  );
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream);
+var suggestion2Stream = createSuggestionStream(responseStream);
+var suggestion3Stream = createSuggestionStream(responseStream);
+
+function renderSuggestion(userData, selector) {
+  var element = document.querySelector(selector);
+  var usernameEl = element.querySelector('.username');
+  usernameEl.href = userData.html_url;
+  usernameEl.textContent = userData.login;
+  var imgEl = element.querySelector('img');
+  imgEl.src = userData.avatar_url;
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
